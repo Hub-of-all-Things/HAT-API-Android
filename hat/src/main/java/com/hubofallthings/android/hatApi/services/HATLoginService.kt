10256e51078e8342a8ac26b5060ebd93841e6b39
leymytel <leymytel@gmail.com>
2019-06-21 17:47:55 +0100

    Changes the process endpoint to lookup for the Hat login

--- hat/src/main/java/com/hubofallthings/android/hatApi/services/HATLoginService.kt
+++ hat/src/main/java/com/hubofallthings/android/hatApi/services/HATLoginService.kt
@@ -2,36 +2,46 @@ package com.hubofallthings.android.hatApi.services
 
 import com.hubofallthings.android.hatApi.managers.HATNetworkManager
 
-data class LoginResolveSuccess(
+data class LoginLookupSuccess(
         val hatName: String,
         val hatCluster: String
 )
 
-data class LoginResolveError(
+data class LoginLookupError(
         val message: String,
         val cause: String
 )
 
 class HATLoginService {
+
     // MARK: - Validate Data
 
     /**
-    Resolves Email address with the HAT
+    Lookup for HAT address with the HAT
 
     - parameter baseUrl: The base url to validate with the HAT f.e https://hatters.hubat.net
-    - parameter email: The email to validate with the HAT
+    - parameter username: The username to lookup with the HAT. Optional
+    - parameter email: The email to lookup with the HAT. Optional
     - parameter sandbox: If the application is sandbox or not
     - parameter successfulCallBack: A function to call if everything is ok and returns hatName and hatCluster
     - parameter failCallBack: A function to call if fail
      */
-    fun resolvesEmailAddress(baseUrl: String, email: String, sandbox: Boolean? = false, successfulCallBack: (LoginResolveSuccess) -> Unit, failCallBack: (LoginResolveError) -> Unit) {
-        val url = "$baseUrl/api/resolve-email"
-        val parameters = listOf("email" to email, "sandbox" to sandbox.toString())
+    fun hatLookup(baseUrl: String, username: String? = null, email: String? = null, sandbox: Boolean? = false, successfulCallBack: (LoginLookupSuccess) -> Unit, failCallBack: (LoginLookupError) -> Unit) {
+        val url = "$baseUrl/api/hat/lookup"
+        val parameters = mutableListOf("sandbox" to sandbox.toString())
+
+        if (!username.isNullOrEmpty()) {
+            parameters.add("name" to username)
+        }
+
+        if (!email.isNullOrEmpty()) {
+            parameters.add("email" to email)
+        }
 
         HATNetworkManager().getRequest(url, parameters, null) {
             if (it?.statusCode == 200) {
                 if (it.json == null) {
-                    failCallBack(LoginResolveError("Not found", "Email not found"))
+                    failCallBack(LoginLookupError("Lookup failed", "Neither email nor HAT name provided"))
                 }
 
                 it.json?.let { json ->
@@ -39,9 +49,9 @@ class HATLoginService {
                     val hatName: String? = json.obj()["hatName"].toString()
 
                     if (!hatName.isNullOrEmpty() && !hatCluster.isNullOrEmpty()) {
-                        successfulCallBack(LoginResolveSuccess(hatName, hatCluster))
+                        successfulCallBack(LoginLookupSuccess(hatName, hatCluster))
                     } else {
-                        failCallBack(LoginResolveError("Not found", "Email not found"))
+                        failCallBack(LoginLookupError("Lookup failed", "Neither email nor HAT name provided"))
                     }
                 }
             } else {
@@ -49,58 +59,13 @@ class HATLoginService {
                     val message: String? = json.obj()["message"].toString()
                     val cause: String? = json.obj()["cause"].toString()
 
-                    failCallBack(LoginResolveError(message ?: "Not found", cause ?: "Unexpected error"))
+                    failCallBack(LoginLookupError(message ?: "Lookup failed", cause ?: "Unexpected error"))
                 }
 
                 if (it?.json == null) {
-                    failCallBack(LoginResolveError("Not found", "Email not found"))
+                    failCallBack(LoginLookupError("Lookup failed", "Neither email nor HAT name provided"))
                 }
             }
         }
     }
-
-    /**
-    Resolves HAT address with HAT
-
-
-    - parameter baseUrl: The base url to validate with the HAT f.e https://hatters.hubat.net
-    - parameter name: The hat name to validate with the HAT
-    - parameter sandbox: If the application is sandbox or not
-    - parameter successfulCallBack: A function to call if everything is ok and returns hatName and hatCluster
-    - parameter failCallBack: A function to call if fail
-     */
-    fun resolveHATAddress(baseUrl: String, name: String, sandbox: Boolean? = false, successfulCallBack: (LoginResolveSuccess) -> Unit, failCallBack: (LoginResolveError) -> Unit) {
-        val url: String = "$baseUrl/api/resolve-hatname"
-        val parameters = listOf("name" to name, "sandbox" to sandbox.toString())
-
-        HATNetworkManager().getRequest(url, parameters, null) {
-            if (it?.statusCode == 200) {
-                if (it.json == null) {
-                    failCallBack(LoginResolveError("Not found", "Email not found"))
-                }
-
-                it.json?.let { json ->
-                    val hatCluster: String? = json.obj()["hatCluster"].toString()
-                    val hatName: String? = json.obj()["hatName"].toString()
-
-                    if (!hatName.isNullOrEmpty() && !hatCluster.isNullOrEmpty()) {
-                        successfulCallBack(LoginResolveSuccess(hatName, hatCluster))
-                    } else {
-                        failCallBack(LoginResolveError("Not found", "Email not found"))
-                    }
-                }
-            } else {
-                it?.json?.let { json ->
-                    val message: String? = json.obj()["message"].toString()
-                    val cause: String? = json.obj()["cause"].toString()
-                    failCallBack(LoginResolveError(message ?: "Not found", cause ?: "Unexpected error"))
-                }
-
-                if (it?.json == null) {
-                    failCallBack(LoginResolveError("Not found", "Email not found"))
-                }
-            }
-        }
-    }
-
 }
