005b5bb946ff691ac17dd32bfe1ed15e17a18100
leymytel <leymytel@gmail.com>
2019-09-04 13:17:39 +0100

    Fixes build issues

--- hat/src/androidTest/java/com/hubofallthings/android/hatApi/HATTestExternalAppsService.kt
+++ hat/src/androidTest/java/com/hubofallthings/android/hatApi/HATTestExternalAppsService.kt
@@ -9,6 +9,9 @@ import org.junit.Assert
 import org.junit.Test
 
 class HATTestExternalAppsService : ExternalAppsService {
+    override fun getAppWithAppId(userToken: String, userDomain: String, applicationId: String, completion: (HATApplicationObject?, String?) -> Unit, failCallBack: (HATError) -> Unit) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     private val mockAppsObj = HATApplicationObject(HATExternalAppsObject("testapp", HATExternalAppsKindObject("", "", "App"), null, HATExternalAppsInfoDeveloperObject("", "", "", ""), null, null, null))
     private val mockAppsObjDataPlug = HATApplicationObject(HATExternalAppsObject("testDataPlug", HATExternalAppsKindObject("", "", "DataPlug"), null, HATExternalAppsInfoDeveloperObject("", "", "", ""), null, null, null))
